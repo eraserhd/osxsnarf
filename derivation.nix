75411a5e0b3f7bac2dc59f6ccf2bc7210a78bb6d
Jason Felice <jason.m.felice@gmail.com>
2019-09-29 12:31:37 -0400

    Use placeholder builtin

--- derivation.nix
+++ derivation.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   src = ./.;
 
   buildInputs = [ plan9port darwin.apple_sdk.frameworks.Carbon ];
-  makeFlags = [ "prefix=$(out)" ];
+  makeFlags = [ "prefix=${placeholder "out"}" ];
 
   meta = with lib; {
     description = "A Plan 9-inspired way to share your OS X clipboard.";
